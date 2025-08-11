d7b970ac19f8e6905045f307ec723728bd96ef8e
mmm <baluevanasty@gmail.com>
2025-08-11 19:11:26 +0200

    Added textfield and 80% of buttons

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,56 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel textPanel = new JPanel();
+        JTextField operationTextField = new JTextField();
 
+        JPanel numPanel = new JPanel();
 
+//        JPanel historyPanel = new JPanel();
+        /// ------- ///
+        frame.setSize(new Dimension(400,700));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setResizable(false);
+        //
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(operationTextField, BorderLayout.CENTER);
+        textPanel.setPreferredSize(new Dimension(400,75));
+        Border line = BorderFactory.createLineBorder(Color.black,1);
+        Border padding = new EmptyBorder(10,10,10,10);
+        operationTextField.setBorder(new CompoundBorder(padding,line));
+        operationTextField.setHorizontalAlignment(JTextField.RIGHT);
+        operationTextField.setFont(new Font("Arial",Font.BOLD, 32));
+        numPanel.setBackground(new Color(0,200,200));
+        numPanel.setPreferredSize(new Dimension(400,50));
+        numPanel.setLayout(new GridLayout(7,5));
+
+        String[] numPanelButtons=
+                {
+                "MS","MR","MC","M+","M-",
+                "2nd","Pi","e","","",
+                "x²","(",")","n!","/",
+                "","7","8","9","*",
+                "10²","4","5","6","-",
+                "log","1","2","3","+",
+                "ln","+/-","0","C","="
+                };
+        for (String label : numPanelButtons) {
+            JButton btn = new JButton(label);
+//            buttonMap.put(label, btn);
+            numPanel.add(btn);
+        }
+
+        frame.add(textPanel, BorderLayout.NORTH);
+        frame.add(numPanel, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
